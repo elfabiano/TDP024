c5923b1227e56ae376f25e1f0c5627784ea161cf
Fabian Wiberg <wiberg96@gmail.com>
2014-09-30 11:48:47 +0200

    Transaction Logic impl + tests

--- account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/TransactionLogicFacadeImpl.java
+++ account-logic-impl/src/se/liu/ida/tdp024/account/logic/impl/facade/TransactionLogicFacadeImpl.java
@@ -5,17 +5,47 @@
  */
 package se.liu.ida.tdp024.account.logic.impl.facade;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import se.liu.ida.tdp024.account.data.api.entity.Transaction;
 import se.liu.ida.tdp024.account.data.api.facade.TransactionEntityFacade;
 import se.liu.ida.tdp024.account.logic.api.facade.TransactionLogicFacade;
+
 
 /**
  *
  * @author fabwi272
  */
 public class TransactionLogicFacadeImpl implements TransactionLogicFacade {
-    private TransactionEntityFacade transactionEntityFacade;
+    private final TransactionEntityFacade transactionEntityFacade;
     
     public TransactionLogicFacadeImpl(TransactionEntityFacade transactionEntityFacade) {
         this.transactionEntityFacade= transactionEntityFacade;
     }
+
+    @Override
+    public long create(String type, int amount, String status) {
+        return transactionEntityFacade.create(type, amount, new SimpleDateFormat(), status);
+    }
+    
+    @Override
+    public Transaction find(long id) {
+        return transactionEntityFacade.find(id);        
+    }
+    
+    @Override
+    public List<Transaction> findAll() {
+        return transactionEntityFacade.findAll();
+    }
+
+    @Override
+    public void update(long id, String type, int amount, SimpleDateFormat time, String status) {
+        transactionEntityFacade.update(id, type, amount, time, status);
+    }
+
+    @Override
+    public void remove(long id) {
+        transactionEntityFacade.remove(id);
+    }
+
 }
